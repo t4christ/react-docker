65dedb93fdf679780931d2b763c84fb5f1341ed2
t4christ <47334485+t4christ@users.noreply.github.com>
2023-06-27 17:38:53 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum -y install docker
+sudo yum -y install docker python3 python3-pip
 service docker start
 usermod -a -G docker ec2-user
 chkconfig docker on
-pip3 install docker-compose 
+pip3 install docker-compose virtualenv
