f90a2b3a235389dccf6cc33d22f6983bb26aa32e
t4christ <47334485+t4christ@users.noreply.github.com>
2023-08-09 12:28:29 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@ sudo yum update -y
 sudo yum -y install docker python3 python3-pip
 sudo systemctl enable docker
 sudo systemctl start docker
-usermod -aG docker ec2-user
+usermod -aG docker $USER
 sudo newgrp docker
-sudo docker swarm init
+# sudo docker swarm init
 # sudo docker network create --driver=bridge --attachable redis_nw
 # sudo docker network create --driver=bridge --attachable web_nw
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
